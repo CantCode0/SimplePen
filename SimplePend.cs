568b7f522b813227474f28a45bbbae1aa3329339
Quoc Nguyen <coolgunny97@gmail.com>
2022-02-06 18:26:13 -0600

    Euler integrator in place

--- SimplePend.cs
+++ SimplePend.cs
@@ -9,13 +9,40 @@ namespace Sim
     {
         private double len=1.1; // pendulum length
         private double g=9.81; // gravity
-
+        int n=2;
+        private double[] x;  //array of states
+        private double[] f;  //right side of equation
         //--------------------------------------------------------------------
         // constructor
         //--------------------------------------------------------------------
         public SimplePend()
         {
-            Console.WriteLine("Inside constructor");
+            x= new double[n];
+            f= new double[n];
+
+            x[0]= 1.0;
+            x[1]= 0.0;
+        }
+        //--------------------------------------------------------------------
+        // step: perform one intergration step via Euler
+        //--------------------------------------------------------------------
+        public void step(double dt)
+        {
+            rhsFunc(x,f);
+            int i;
+            for(i=0;i<n;++i)
+            {
+                x[i] =x[i]+f[i] *dt;
+            }
+            Console.WriteLine($"{f[0].ToString()} {f[1].ToString()}");
+        }
+        //--------------------------------------------------------------------
+        // rhsFunc: function to calculate right hand side of pendulum
+        //--------------------------------------------------------------------
+        public void rhsFunc(double[] st, double[] ff)
+        {
+            ff[0] = st[1];
+            ff[1] = -g/len* Math.Sin(st[0]);
         }
         //--------------------------------------------------------------------
         // Getters and setters
@@ -40,6 +67,20 @@ namespace Sim
                 if(value>=0.0)
                     g=value;
            }
+        }
+
+        public double theta 
+        {
+            get {return x[0];}
+
+            set {x[0] =value;}
+        }
+
+        public double thetaDot
+        {
+            get {return x[1];}
+
+            set {x[1] =value;}
         }
 
     }
